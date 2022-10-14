c23d1aba62a193ca41884d97707da9c9416eca1a
Akram Hossain <nayanbd17@gmail.com>
2022-10-14 11:14:44 +0600

    html file added

---
+++ resources/html-design/gsore-admin/invoice.php
@@ -0,0 +1,219 @@
+<?php include_once('include/header.php') ?>
+<?php include_once('include/sidebar.php') ?>
+
+ 
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-flex align-items-center justify-content-between">
+                            <h4 class="page-title mb-0 font-size-18">Invoice</h4>
+
+                            <div class="page-title-right">
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                    <li class="breadcrumb-item active">Invoice</li>
+                                </ol>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- end page title -->
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="invoice-title">
+                                    <h4 class="float-end font-size-16">Order # 12345</h4>
+                                    <div class="mb-4">
+                                        <img src="assets/images/logo-dark.png" alt="logo" height="20" />
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <address>
+                                            <strong>Billed To:</strong><br>
+                                            John Smith<br>
+                                            1234 Main<br>
+                                            Apt. 4B<br>
+                                            Springfield, ST 54321
+                                        </address>
+                                    </div>
+                                    <div class="col-6 text-end">
+                                        <address>
+                                            <strong>Shipped To:</strong><br>
+                                            Kenny Rigdon<br>
+                                            1234 Main<br>
+                                            Apt. 4B<br>
+                                            Springfield, ST 54321
+                                        </address>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6 mt-3">
+                                        <address>
+                                            <strong>Payment Method:</strong><br>
+                                            Visa ending **** 4242<br>
+                                            jsmith@email.com
+                                        </address>
+                                    </div>
+                                    <div class="col-6 mt-3 text-end">
+                                        <address>
+                                            <strong>Order Date:</strong><br>
+                                            October 16, 2019<br><br>
+                                        </address>
+                                    </div>
+                                </div>
+                                <div class="py-2 mt-3">
+                                    <h3 class="font-size-15 fw-bold">Order summary</h3>
+                                </div>
+                                <div class="table-responsive">
+                                    <table class="table table-nowrap">
+                                        <thead>
+                                            <tr>
+                                                <th style="width: 70px;">No.</th>
+                                                <th>Item</th>
+                                                <th class="text-end">Price</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>01</td>
+                                                <td>Qovex - Admin & Dashboard Template</td>
+                                                <td class="text-end">$499.00</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>02</td>
+                                                <td>Qovex - Responsive Landing Template</td>
+                                                <td class="text-end">$399.00</td>
+                                            </tr>
+
+                                            <tr>
+                                                <td>03</td>
+                                                <td>Veltrix - Admin & Dashboard Template</td>
+                                                <td class="text-end">$499.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="2" class="text-end">Sub Total</td>
+                                                <td class="text-end">$1397.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="2" class="border-0 text-end">
+                                                    <strong>Shipping</strong>
+                                                </td>
+                                                <td class="border-0 text-end">$13.00</td>
+                                            </tr>
+                                            <tr>
+                                                <td colspan="2" class="border-0 text-end">
+                                                    <strong>Total</strong>
+                                                </td>
+                                                <td class="border-0 text-end">
+                                                    <h4 class="m-0">$1410.00</h4>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="d-print-none">
+                                    <div class="float-end">
+                                        <a href="javascript:window.print()"
+                                            class="btn btn-success waves-effect waves-light"><i
+                                                class="fa fa-print"></i></a>
+                                        <a href="#" class="btn btn-primary w-md waves-effect waves-light">Send</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row -->
+
+            </div>
+            <!-- End Page-content -->
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <script>document.write(new Date().getFullYear())</script> © Qovex.
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="text-sm-end d-none d-sm-block">
+                                Design & Develop by Themesbrand
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+        <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+
+</div>
+<!-- end container-fluid -->
+
+<!-- Right Sidebar -->
+<div class="right-bar">
+    <div data-simplebar class="h-100">
+        <div class="rightbar-title px-3 py-4">
+            <a href="javascript:void(0);" class="right-bar-toggle float-end">
+                <i class="mdi mdi-close noti-icon"></i>
+            </a>
+            <h5 class="m-0">Settings</h5>
+        </div>
+
+        <!-- Settings -->
+        <hr class="mt-0" />
+        <h6 class="text-center mb-0">Choose Layouts</h6>
+
+        <div class="p-4">
+            <div class="mb-2">
+                <img src="assets/images/layouts/layout-1.jpg" class="img-fluid img-thumbnail" alt="">
+            </div>
+
+            <div class="form-check form-switch mb-3">
+                <input type="checkbox" class="form-check-input theme-choice" id="light-mode-switch" checked />
+                <label class="form-check-label" for="light-mode-switch">Light Mode</label>
+            </div>
+
+            <div class="mb-2">
+                <img src="assets/images/layouts/layout-2.jpg" class="img-fluid img-thumbnail" alt="">
+            </div>
+
+            <div class="form-check form-switch mb-3">
+                <input type="checkbox" class="form-check-input theme-choice" id="dark-mode-switch"
+                    data-bsStyle="assets/css/bootstrap-dark.min.css" data-appStyle="assets/css/app-dark.min.css" />
+                <label class="form-check-label" for="dark-mode-switch">Dark Mode</label>
+            </div>
+
+            <div class="mb-2">
+                <img src="assets/images/layouts/layout-3.jpg" class="img-fluid img-thumbnail" alt="">
+            </div>
+            <div class="form-check form-switch mb-5">
+                <input type="checkbox" class="form-check-input theme-choice" id="rtl-mode-switch"
+                    data-appStyle="assets/css/app-rtl.min.css" />
+                <label class="form-check-label" for="rtl-mode-switch">RTL Mode</label>
+            </div>
+
+        </div>
+
+    </div>
+    <!-- end slimscroll-menu-->
+</div>
+<!-- /Right-bar -->
+
+<!-- Right bar overlay-->
+<div class="rightbar-overlay"></div>
+<?php include_once('include/footer.php') ?>
