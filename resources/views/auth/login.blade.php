bcefcdd24ccca63d7793b94b30706386172c42e9
Md Arif <arifypp@gmail.com>
2022-10-15 20:50:23 +0600

    layout and auth login ready

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.fullwidth')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +69,86 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+<div class="home-btn d-none d-sm-block">
+        <a href="index.html" class="text-dark"><i class="fas fa-home h2"></i></a>
+    </div>
+    <div class="account-pages my-5 pt-sm-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="card overflow-hidden">
+                        <div class="bg-login text-center">
+                            <div class="bg-login-overlay"></div>
+                            <div class="position-relative">
+                                <h5 class="text-white font-size-20">Welcome Back !</h5>
+                                <p class="text-white-50 mb-0">Sign in to continue to Qovex.</p>
+                                <a href="index.html" class="logo logo-admin mt-4">
+                                    <img src="{{asset ('backend/assets/images/logo-sm-dark.png') }}" alt="" height="30">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="card-body pt-5">
+                            <div class="p-2">
+                                <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                                    @csrf
+
+                                    <div class="mb-3">
+                                        <label for="email" class="col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Enter Email" required autocomplete="email" autofocus>
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                        
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="password" class="col-form-label text-md-end">{{ __('Password') }}</label>
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Enter password" required autocomplete="current-password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+
+                                    <div class="mt-3">
+                                        <button class="btn btn-primary w-100 waves-effect waves-light" type="submit">Log
+                                            In</button>
+                                    </div>
+
+                                    <div class="mt-4 text-center">
+                                        <a href="{{ route('password.request') }}" class="text-muted"><i
+                                                class="mdi mdi-lock me-1"></i> Forgot your password?</a>
+                                    </div>
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="mt-5 text-center">
+                        <p>Don't have an account ? 
+                            <a href="{{ route('register') }}"
+                                class="fw-medium text-primary"> Signup now </a> </p>
+                        <p>©
+                            <script>document.write(new Date().getFullYear())</script> Qovex. Crafted with <i
+                                class="mdi mdi-heart text-danger"></i> by Themesbrand
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/backend/partials/css.blade.php
@@ -0,0 +1,12 @@
+<!-- jquery.vectormap css -->
+<link href="{{ asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+
+<!-- Bootstrap Css -->
+<link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+<!-- Toastr -->
+<link href="{{ asset('backend/assets/libs/toastr/toastr.min.css') }}" rel="stylesheet" type="text/css" />
+
+<!-- Icons Css -->
+<link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<!-- App Css-->
+<link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
