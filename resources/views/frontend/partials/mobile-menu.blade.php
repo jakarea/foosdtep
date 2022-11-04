0e2f32a97f742c0379cbb280f4b9dcf979f946d6
Akram Hossain <nayanbd17@gmail.com>
2022-11-05 03:08:13 +0600

    design issue fixed

--- resources/views/frontend/partials/mobile-menu.blade.php
+++ resources/views/frontend/partials/mobile-menu.blade.php
@@ -16,7 +16,7 @@
                         <ul class="header__mobile--rightside header__user-action-icon  d-flex align-items-center justify-content-end"> 
                             <!-- Start Header Add Cart Box -->
                             <li>
-                                <a href="#offcanvas-add-cart__box" class="offcanvas-toggle">
+                                <a href="{{ route('product.cart') }}">
                                     <i class="icon-shopping-cart"></i>
                                     <span class="wishlist-item-count pos-absolute">3</span>
                                 </a>
@@ -25,245 +25,38 @@
 
                         </ul>   <!-- End User Action -->
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <div class="header-menu-vertical pos-relative m-t-30">
-                            <h4 class="menu-title link--icon-left"><i class="far fa-align-left"></i>CATEGORIES</h4>
-                            <ul class="menu-content pos-absolute">
-                                <li class="menu-item"><a href="#">Search Terms</a></li>
-                                <li class="menu-item"><a href="#">Advanced Search</a></li>
-                                <li class="menu-item"><a href="#">Helps & Faqs</a></li>
-                                <li class="menu-item"><a href="#">Store Location</a></li>
-                                <li class="menu-item"><a href="#"> Orders & Returns</a></li>
-                                <li class="menu-item"><a href="#"> Deliveries</a></li>
-                                <li class="menu-item"><a href="#"> Refund & Returns</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                </div> 
             </div>
         </div> <!--  Start Mobile Header Section   -->
 
         <!--  Start Mobile-offcanvas Menu Section   -->
         <div id="offcanvas-mobile-menu" class="offcanvas offcanvas-mobile-menu">
             <div class="offcanvas__top">
-                <span class="offcanvas__top-text"></span>
+                <span class="offcanvas__top-text">Menu</span>
                 <button class="offcanvas-close"><i class="fal fa-times"></i></button>
             </div>
             
-            <div class="offcanvas-inner">
-                <ul class="user-set-role d-flex justify-content-between m-tb-15">
-                    <li class="user-currency pos-relative">
-                        <a class="user-set-role__button" href="#" data-bs-toggle="dropdown" aria-expanded="false">Select Language<i class="fal fa-chevron-down"></i></a>
-                        <ul class="expand-dropdown-menu dropdown-menu" >
-                            <li><a href="#"><img src="assets/img/icon/flag/icon_usa.png" alt="">English</a></li>
-                            <li><a href="#"><img src="assets/img/icon/flag/icon_france.png" alt="">French</a></li>
-                        </ul>
-                    </li>
-                    <li class="user-info pos-relative">
-                        <a class="user-set-role__button" href="#" data-bs-toggle="dropdown" aria-expanded="false" >Select Currency <i class="fal fa-chevron-down"></i></a>
-                        <ul class="expand-dropdown-menu dropdown-menu" >
-                            <li><a href="#">USD</a></li>
-                            <li><a href="#">POUND</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="header-search m-tb-15" action="#" method="post">
-                    <div class="header-search__content pos-relative">
-                        <input type="search" name="header-search" placeholder="Search our store" required>
-                        <button class="pos-absolute" type="submit"><i class="icon-search"></i></button>
-                    </div>
-                </form>
+            <div class="offcanvas-inner">  
                  <!-- Start Mobile User Action -->
-                <ul class="header__user-action-icon m-tb-15 text-center">
-                    <!-- Start Header Wishlist Box -->
-                    <li>
-                        <a href="#">
-                            <i class="icon-users"></i>
-                        </a>
-                    </li> <!-- End Header Wishlist Box -->
-                    <!-- Start Header Wishlist Box -->
-                    <li>
-                        <a href="#">
-                            <i class="icon-heart"></i>
-                            <span class="item-count pos-absolute">3</span>
-                        </a>
-                    </li> <!-- End Header Wishlist Box -->
-                    <!-- Start Header Add Cart Box -->
-                    <li>
-                        <a href="#">
-                            <i class="icon-shopping-cart"></i>
-                            <span class="wishlist-item-count pos-absolute">3</span>
-                        </a>
-                    </li> <!-- End Header Add Cart Box -->
-                </ul>  <!-- End Mobile User Action -->
+                 
                 <div class="offcanvas-menu">
                     <ul>
-                        <li><a href="{{url('/')}}"><span>Home</span></a></li>
+                        <li> <a href="{{ url('/') }}" >Home</a></li>
                         <li>
-                            <a href="#"><span>Shop</span></a>
-                            <ul class="sub-menu">
-                                <li>
-                                    <a href="#">Shop Layout</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="{{url('/')}}">Grid Left Sidebar</a></li>
-                                        <li><a href="{{url('/')}}">Grid Right Sidebar</a></li>
-                                        <li><a href="{{url('/')}}">Full Width</a></li>
-                                        <li><a href="{{url('/')}}">List Left Sidebar</a></li>
-                                        <li><a href="{{url('/')}}">List Right Sidebar</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                            <ul class="sub-menu">
-                                <li>
-                                    <a href="#">Shop Pages</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="{{url('/')}}">Cart</a></li>
-                                        <li><a href="{{url('/')}}">Checkout</a></li>
-                                        <li><a href="{{url('/')}}">Compare</a></li>
-                                        <li><a href="{{url('/')}}">Empty Cart</a></li>
-                                        <li><a href="{{url('/')}}">Wishlist</a></li>
-                                        <li><a href="{{url('/')}}">My Account</a></li>
-                                        <li><a href="{{url('/')}}">Login</a></li>
-                                                        
-                                    </ul>
-                                </li>
-                            </ul>
-                            <ul class="sub-menu">
-                                <li>
-                                    <a href="#">Product Single</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="#">Simple</a></li>
-                                        <li><a href="#">Affiliate</a></li>
-                                        <li><a href="{{url('/')}}">Grouped</a></li>
-                                        <li><a href="{{url('/')}}">Variable</a></li>
-                                        <li><a href="{{url('/')}}">Left Tab</a></li>
-                                        <li><a href="{{url('/')}}">Right Tab</a></li>
-                                        <li><a href="{{url('/')}}">Single Slider</a></li>
-                                        <li><a href="{{url('/')}}">Gallery Left</a></li>
-                                        <li><a href="{{url('/')}}">Gallery Right</a></li>
-                                        <li><a href="{{url('/')}}">Sticky Left</a></li>
-                                        <li><a href="{{url('/')}}>Sticky Right</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
+                            <a href="{{ url('/about') }}">About Us</a>
+                        </li> <!-- End Single Nav link-->
+
+                        <!--Start Single Nav link-->
                         <li>
-                            <a href="#"><span>Blogs</span></a>
-                            <ul class="sub-menu">
-                                <li>
-                                    <a href="#">Blog Grid</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="{{url('/')}}"> Blog Grid Left Sidebar</a></li>
-                                        <li><a href="{{url('/')}}"> Blog Grid Right Sidebar</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Blog List</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="{{url('/')}}"> Blog List Left Sidebar</a></li>
-                                        <li><a href="{{url('/')}}"> Blog List Right Sidebar</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#">Blog Single</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="{{url('/')}}"> Blog Single Left Sidebar</a></li>
-                                        <li><a href="{{url('/')}}"> Blog Single Right Sidebar</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </li>
+                                <a href="{{ route('products') }}">Products</a>
+                        </li> <!-- End Single Nav link--> 
+                        
+                        <!--Start Single Nav link-->
                         <li>
-                            <a href="#"><span>Pages</span></a>
-                            <ul class="sub-menu">
-                                <li><a href="{{url('/about')}}">About Us</a></li>
-                                <li><a href="{{url('/')}}">Frequently Questions</a></li>
-                                <li><a href="{{url('/')}}">Privacy Policy</a></li>
-                                <li><a href="{{url('/')}}">404 Page</a></li>
-                            </ul>
+                                <a href="{{ url('/contact') }}">Contact</a>
                         </li>
-                        <li><a href="{{url('/contact')}}">Contact Us</a></li>
                     </ul>
                 </div>
-            </div>
-            <ul class="offcanvas__social-nav m-t-50">
-                <li class="offcanvas__social-list"><a href="#" class="offcanvas__social-link"><i class="fab fa-facebook-f"></i></a></li>
-                <li class="offcanvas__social-list"><a href="#" class="offcanvas__social-link"><i class="fab fa-twitter"></i></a></li>
-                <li class="offcanvas__social-list"><a href="#" class="offcanvas__social-link"><i class="fab fa-youtube"></i></a></li>
-                <li class="offcanvas__social-list"><a href="#" class="offcanvas__social-link"><i class="fab fa-google-plus-g"></i></a></li>
-                <li class="offcanvas__social-list"><a href="#" class="offcanvas__social-link"><i class="fab fa-instagram"></i></a></li>
-            </ul>
+            </div> 
         </div> <!--  End Mobile-offcanvas Menu Section   -->
-
-        <!--  Start Popup Add Cart  -->
-        <div  id="offcanvas-add-cart__box" class="offcanvas offcanvas-cart offcanvas-add-cart">
-            <div class="offcanvas__top">
-                <span class="offcanvas__top-text"><i class="icon-shopping-cart"></i>Cart</span>
-                <button class="offcanvas-close"><i class="fal fa-times"></i></button>
-            </div>
-            <!-- Start Add Cart Item Box-->
-            <ul class="offcanvas-add-cart__menu">
-                <!-- Start Single Add Cart Item-->
-                <li class="offcanvas-add-cart__list pos-relative d-flex align-items-center justify-content-between ">
-                    <div class="offcanvas-add-cart__content d-flex align-items-start m-r-10">
-                        <div class="offcanvas-add-cart__img-box pos-relative">
-                            <a href="#" class="offcanvas-add-cart__img-link img-responsive"><img src="assets/img/product/size-small/product-home-1-img-1.jpg" alt="" class="add-cart__img"></a>
-                            <span class="offcanvas-add-cart__item-count pos-absolute">2x</span>
-                        </div>
-                        <div class="offcanvas-add-cart__detail">
-                            <a href="#" class="offcanvas-add-cart__link">Lucky Wooden Elephant</a>
-                            <span class="offcanvas-add-cart__price">$29.00</span>
-                            <span class="offcanvas-add-cart__info">Dimension: 40x60cm</span>
-                        </div>
-                    </div>
-                    <button class="offcanvas-add-cart__item-dismiss"><i class="fal fa-times"></i></button>
-                </li> <!-- Start Single Add Cart Item-->
-                <!-- Start Single Add Cart Item-->
-                <li class="offcanvas-add-cart__list pos-relative d-flex align-items-center justify-content-between">
-                    <div class="offcanvas-add-cart__content d-flex  align-items-start m-r-10">
-                        <div class="offcanvas-add-cart__img-box pos-relative">
-                            <a href="#" class="offcanvas-add-cart__img-link img-responsive"><img src="assets/img/product/size-small/product-home-1-img-2.jpg" alt="" class="add-cart__img"></a>
-                            <span class="offcanvas-add-cart__item-count pos-absolute">2x</span>
-                        </div>
-                        <div class="offcanvas-add-cart__detail">
-                            <a href="#" class="offcanvas-add-cart__link">Lucky Wooden Elephant</a>
-                            <span class="offcanvas-add-cart__price">$29.00</span>
-                            <span class="offcanvas-add-cart__info">Dimension: 40x60cm</span>
-                        </div>
-                    </div>
-                    <button class="offcanvas-add-cart__item-dismiss"><i class="fal fa-times"></i></button>
-                </li> <!-- Start Single Add Cart Item-->
-            </ul> <!-- Start Add Cart Item Box-->
-            <!-- Start Add Cart Checkout Box-->
-            <div class="offcanvas-add-cart__checkout-box-bottom">
-                <!-- Start offcanvas Add Cart Checkout Info-->
-                <ul class="offcanvas-add-cart__checkout-info">
-                    <!-- Start Single Add Cart Checkout Info-->
-                    <li class="offcanvas-add-cart__checkout-list">
-                        <span class="offcanvas-add-cart__checkout-left-info">Subtotal</span>
-                        <span class="offcanvas-add-cart__checkout-right-info">$60.59</span>
-                    </li> <!-- End Single Add Cart Checkout Info-->
-                    <!-- Start Single Add Cart Checkout Info-->
-                    <li class="offcanvas-add-cart__checkout-list">
-                        <span class="offcanvas-add-cart__checkout-left-info">Shipping</span>
-                        <span class="offcanvas-add-cart__checkout-right-info">$7.00</span>
-                    </li> <!-- End Single Add Cart Checkout Info-->
-                    <!-- Start Single Add Cart Checkout Info-->
-                    <li class="offcanvas-add-cart__checkout-list">
-                        <span class="offcanvas-add-cart__checkout-left-info">Taxes</span>
-                        <span class="offcanvas-add-cart__checkout-right-info">$0.00</span>
-                    </li> <!-- End Single Add Cart Checkout Info-->
-                    <!-- Start Single Add Cart Checkout Info-->
-                    <li class="offcanvas-add-cart__checkout-list">
-                        <span class="offcanvas-add-cart__checkout-left-info">Total</span>
-                        <span class="offcanvas-add-cart__checkout-right-info">$67.59</span>
-                    </li> <!-- End Single Add Cart Checkout Info-->
-                </ul> <!-- End offcanvas Add Cart Checkout Info-->
-
-                <div class="offcanvas-add-cart__btn-checkout">
-                    <a href="{{url('/')}}" class="btn btn--block btn--radius btn--box btn--black btn--black-hover-green btn--large btn--uppercase font--bold">Checkout</a>
-                </div>
-            </div> <!-- End Add Cart Checkout Box-->
-        </div> <!-- End Popup Add Cart -->
+ 
