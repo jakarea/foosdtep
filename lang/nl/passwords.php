f9a00bd9e0a3e649ed96bcfdd46bd489158a2c31
admin <admin>
2022-11-07 15:36:30 +0600

    lang file added

---
+++ lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Je wachtwoord is greset',
+    'sent' => 'We hebben je een e-mail verzonden met instructies.',
+    'throttled' => 'Wacht een aantal seconden voor je het opnieuw probeert.',
+    'token' => 'Het wachtwoord token dat je hebt ingevuld is incorrect.',
+    'user' => "We kunnen geen gebruiker vinden met dit email adres.",
+
+];
---
+++ lang/nl/text.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'menu' => 'Menu',
+    'home' => 'Home',
+    'about_us' => 'Over Ons',
+    'products' => 'Producten',
+    'contacts' => 'Contact',
+    'top_cats' => 'Top categorie(en)',
+    'special_discount' => 'Speciale kortingen',
+    'for_all_grocery' => 'Voor iedereen',
+    '20_off' => 'Begin vandaag op Foodsteps!',
+    'shop_now' => 'Shop nu',
+    'our_latest_news' => 'Onze Laatste Nieuws',
+    'subscribe_us' => 'Schrijf je in voor onze nieuwsbrief',
+    'your_email_address' => 'Jouw e-mail adres',
+    'subscribe' => 'Abonneren',
+    'categories' => 'Categorieen',
+    'category' => 'Categorie',
+    'crafted_with' => 'Crafted with',
+    'brand' => 'Brand',
+    'group' => 'Group',
+    'faith' => 'Faith',
+    'lines' => 'Lines',
+    'contents' => 'Contents',
+    'allerge_and_diet' => 'Allergens & Diet Preference',
+    'sort_by' => 'Sorter bij:',
+    'relevance' => 'Relevantie',
+    'name_a_z' => 'Naam, A-Z',
+    'name_z_a' => 'Naam, Z-A ',
+    'price_low_high' => 'Prijs, laag naar hoog',
+    'price_high_low' => 'Prijs, hoog naar laag',
+    'showing' => 'Laten zien',
+    'of' => 'van',
+    'result' => 'resultaten',
+    'share_this_product' => 'Deel dit product',
+    'add_to_cart' => 'Toevoegen aan winkelmand',
+    'prev' => ' Vorige',
+    'previous' => 'Vorige',
+    'next' => 'Volgende'
+
+];
